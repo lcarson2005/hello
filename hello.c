bc03337ad4ac75322f08e02de58be89aaf67e0e3
Lane Carson <lcarson3@unl.edu>
2024-08-29 17:25:16 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Lane Carson
+ *         lcarson3@unl.edu
+ * Date: 8/27/2024
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Computer Engineering!\n");
+  printf("Lane Carson!\n");
+
+  return 0;
+}
